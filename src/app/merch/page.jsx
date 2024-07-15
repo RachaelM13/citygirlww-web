3256a9f14738f94c0b038bc5b600770e0987d9a6
RachaelM13 <rachael.m.mccann@vanderbilt.edu>
2024-07-15 12:48:48 -0400

    updates merch page added socials walls

--- src/app/merch/page.jsx
+++ src/app/merch/page.jsx
@@ -1,7 +1,35 @@
+"use client";
+
+import React from "react";
+import styles from "./page.module.css";
+
 export default function Merch() {
   return (
-    <main>
-      <h2>Merch Page</h2>
+    <main className={styles.body}>
+      <h1 className={styles.h1}>SUPPORT us!</h1>
+
+      <a
+        href="https://www.bonfire.com/store/cgww-pgh/?utm_source=copy_link&utm_medium=store_page_published_share&utm_campaign=cgww-pgh&utm_content=default"
+        target="_blank"
+        rel="noopener noreferrer"
+        style={{ textDecoration: "none", color: "inherit" }}
+      >
+        <div
+          className={styles.card}
+          onMouseOver={(e) =>
+            (e.currentTarget.style.boxShadow = "0 8px 16px rgba(0, 0, 0, 0.2)")
+          }
+          onMouseOut={(e) =>
+            (e.currentTarget.style.boxShadow = "0 4px 8px rgba(0, 0, 0, 0.1)")
+          }
+        >
+          <img
+            src="BonfireMerch.png" // Replace with a relevant image URL
+            alt="Bonfire Store"
+            style={{ width: "600px", height: "auto", objectFit: "cover" }}
+          />
+        </div>
+      </a>
     </main>
   );
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,8 +2,6 @@
 import React, { useEffect, useState } from "react";
 import styles from "./page.module.css";
 import Image from "next/image";
-// components
-import GalleryStrip from "./components/GalleryStrip";
 
 export default function Home() {
   const [isMobile, setIsMobile] = useState(false);
@@ -22,6 +20,28 @@ export default function Home() {
     // Clean up the event listener on component unmount
     return () => {
       window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.type = "text/javascript";
+    script.async = true;
+    script.charset = "UTF-8";
+    script.src =
+      "https://cdn.curator.io/published/373188f4-2f78-4a95-9e7e-71b64839335b.js";
+
+    const insertScript = () => {
+      const s = document.getElementsByTagName("script")[0];
+      s.parentNode.insertBefore(script, s);
+    };
+
+    insertScript();
+
+    return () => {
+      if (script.parentNode) {
+        script.parentNode.removeChild(script);
+      }
     };
   }, []);
 
@@ -46,7 +66,11 @@ export default function Home() {
         </div>
         <div className={styles.blob}>
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 310 350">
-            <path d="M156.4,339.5c31.8-2.5,59.4-26.8,80.2-48.5c28.3-29.5,40.5-47,56.1-85.1c14-34.3,20.7-75.6,2.3-111  c-18.1-34.8-55.7-58-90.4-72.3c-11.7-4.8-24.1-8.8-36.8-11.5l-0.9-0.9l-0.6,0.6c-27.7-5.8-56.6-6-82.4,3c-38.8,13.6-64,48.8-66.8,90.3c-3,43.9,17.8,88.3,33.7,128.8c5.3,13.5,10.4,27.1,14.9,40.9C77.5,309.9,111,343,156.4,339.5z" />
+            <path
+              d="M156.4,339.5c31.8-2.5,59.4-26.8,80.2-48.5c28.3-29.5,40.5-47,56.1-85.1c14-34.3,20.7-75.6,2.3-111
+              c-18.1-34.8-55.7-58-90.4-72.3c-11.7-4.8-24.1-8.8-36.8-11.5l-0.9-0.9l-0.6,0.6c-27.7-5.8-56.6-6-82.4,3
+              c-38.8,13.6-64,48.8-66.8,90.3c-3,43.9,17.8,88.3,33.7,128.8c5.3,13.5,10.4,27.1,14.9,40.9C77.5,309.9,111,343,156.4,339.5z"
+            />
           </svg>
         </div>
       </div>
@@ -67,17 +91,18 @@ export default function Home() {
         </div>
       )}
 
-      {isMobile ? (
-        <Image
-          src="/CGWWgroup2.jpg"
-          alt="Mobile View"
-          layout="responsive"
-          width={200}
-          height={100}
-        />
-      ) : (
-        <GalleryStrip />
-      )}
+      {/* insta images */}
+      <div className={styles.carouselStrip}>
+        <div id="curator-feed-new-feed-layout">
+          <a
+            href="https://curator.io"
+            target="_blank"
+            className="crt-logo crt-tag"
+          >
+            Powered by Curator.io
+          </a>
+        </div>
+      </div>
 
       {/* strip3 Contact US */}
       <div className={styles.strip3}>
